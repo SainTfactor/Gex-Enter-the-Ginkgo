e9df0539a586bcb1754e044784759c7d0f005ee1
Sean Tyler Atwell <sta0003@auburn.edu>
2018-05-16 16:40:40 -0400

    Update gex_script.user.js

--- gex_script.user.js
+++ gex_script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gex, Enter the Ginkgo
 // @namespace    https://github.com/SainTfactor/Gex-Enter-the-Ginkgo
-// @version      0.1.9.1
+// @version      0.1.9.2
 // @description  Making Ginkgo not suck again!
 // @author       @SainTfactor
 // @match        http://ginkgo.azuretitan.com/*resume_course*
